20a13d408b9ab9ad999b1240886ce74d59eb4872
Suzuran Hana <spark-ge@qq.com>
2023-12-05 10:00:10 +0800

    allows edit config by serial

--- ESP8266_Network_Clock_7SEGMAX7219.ino
+++ ESP8266_Network_Clock_7SEGMAX7219.ino
@@ -9,12 +9,16 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <SPI.h>
-//---------------修改此处""内的信息---------------------------------------------
-const char ssid[] = "CU_Ve4J";                        // WiFi名
-const char pass[] = "123456788";                      // WiFi密码
-static const char ntpServerName[] = "ntp.aliyun.com"; // NTP服务器，上海交通大学
-const int timeZone = 0;                               // 时区，北京时间为+8
-//-----------------------------------------------------------------------------
+#include <EEPROM.h>
+
+String ssid = "OnePlus Ace Pro Genshin Version"; // WiFi名
+String pass = "password";						// WiFi密码
+String ntpServerName = "ntp.aliyun.com";			// NTP服务器，上海交通大学
+int timeZone = 8;								// 时区，北京时间为+8
+// char config[128] = "CU_Ve4J;123456788;ntp.aliyun.com;8;"; // max 127
+char config[128] = "OnePlus Ace Pro Genshin Version;password;ntp.aliyun.com;8;"; // max 127
+// config format: SSID;password;ntp server;timezone;
+
 WiFiUDP Udp;
 unsigned int localPort = 8888; // 用于侦听UDP数据包的本地端口
 const int slaveSelect = 5;
@@ -33,14 +37,18 @@ void setup()
         continue;
     Serial.println("NTP Clock version v1.0");
 
+    EEPROM.begin(128);
+    loadConfig();
+    applyConfig();
+
     SPI.begin();
     pinMode(slaveSelect, OUTPUT);
     digitalWrite(slaveSelect, LOW);
-    sendCommand(12, 1);         // Shutdown,open
-    sendCommand(15, 0);         // DisplayTest,no
-    sendCommand(10, 15);        // Intensity,15(max)
+    sendCommand(12, 1);			// Shutdown,open
+    sendCommand(15, 0);			// DisplayTest,no
+    sendCommand(10, 5);		// Intensity,15(max)
     sendCommand(11, scanLimit); // ScanLimit,8-1=7
-    sendCommand(9, 255);        // DecodeMode,Code B decode for digits 7-0
+    sendCommand(9, 255);		// DecodeMode,Code B decode for digits 7-0
     digitalWrite(slaveSelect, HIGH);
     initdisplay();
     Serial.println("LED Ready");
@@ -48,11 +56,21 @@ void setup()
     Serial.print("Connecting WiFi...");
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED)
+    int i = 0;
+    for (int i = 0; i < 20; i++)
+    // while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.print(".");
+        if (WiFi.status() == WL_CONNECTED)
+            goto connected;
     }
+    Serial.println("WiFi connect failed. ");
+    for (;;)
+        if (Serial.available() > 0)
+            SerialEvent();
+
+connected:
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
@@ -78,6 +96,8 @@ void loop()
             digitalClockDisplay();
         }
     }
+    if (Serial.available() > 0)
+        SerialEvent();
 }
 
 void sendCommand(int command, int value)
@@ -111,6 +131,7 @@ void digitalClockDisplay()
     seconds = second();
     weekdays = weekday();
     Serial.printf("%d/%d/%d %d:%d:%d Weekday:%d\n", years, months, days, hours, minutes, seconds, weekdays);
+    Serial.println();
 
     // sendCommand(8, hours / 10);
     // sendCommand(7, hours % 10);
@@ -122,22 +143,109 @@ void digitalClockDisplay()
     // sendCommand(1, seconds % 10);
 
     sendCommand(8, months / 10);
-    sendCommand(7, months % 10);
+    sendCommand(7, (months % 10) | 0x80);
     sendCommand(6, days / 10);
     sendCommand(5, days % 10);
 
-    sendCommand(4, minutes / 10);
-    sendCommand(3, minutes % 10 + 128);
-    sendCommand(2, seconds / 10);
-    sendCommand(1, seconds % 10);
+    sendCommand(4, hours / 10);
+    // sendCommand(3, hours % 10 + 128);
+    sendCommand(3, (hours % 10) | 0x80);
+    sendCommand(2, minutes / 10);
+    sendCommand(1, minutes % 10);
 
     delay(500);
-    sendCommand(3, minutes % 10);
+    sendCommand(3, hours % 10);
 }
 
-/*-------- NTP 代码 ----------*/
+void SerialEvent()
+{
+    String command;
+    while (Serial.available() > 0)
+    {
+        command += char(Serial.read());
+        delay(10); // 延时函数用于等待字符完全进入缓冲区，可以尝试没有延时，输出结果会是什么
+    }
+    Serial.print(">");
+    Serial.println(command);
+    if (command == "load")
+        loadConfig();
+    if (command == "apply")
+        applyConfig();
+    else if (command == "edit")
+        editConfig();
+    else if (command == "help")
+    {
+        const String helpText = "Available commands: help edit load apply\r\nSSID;password;ntp server;timezone;";
+        Serial.println(helpText);
+    }
+}
 
-const int NTP_PACKET_SIZE = 48;     // NTP时间在消息的前48个字节里
+void loadConfig()
+{
+    EEPROM.get(0, config);
+    Serial.print("read config: ");
+    Serial.println(config);
+    return;
+}
+
+void editConfig()
+{
+    Serial.println("pls. send config");
+    // config = "";
+    while (!Serial.available() > 0)
+        continue;
+    for (int i = 0; i < 127; i++)
+    {
+        if (Serial.available() > 0)
+            config[i] = char(Serial.read());
+        else
+            config[i] = 0x00;
+        delay(10);
+    }
+
+    if (config == "exit")
+        return;
+    Serial.print("edit config: ");
+    Serial.println(config);
+
+    EEPROM.put(0, config);
+    EEPROM.commit();
+}
+
+void applyConfig()
+{
+    String templist;
+    String temp(config);
+
+    // const char *ssid = temp.substring(0, temp.indexOf(";")).data();
+    // strcpy(ssid, temp.substring(0, temp.indexOf(";")).c_str());
+    ssid = temp.substring(0, temp.indexOf(";"));
+    Serial.print("SSID: ");
+    Serial.println(ssid);
+    temp = temp.substring(temp.indexOf(";") + 1);
+
+    // pass = (char *)temp.substring(0, temp.indexOf(";")).c_str();
+    pass = temp.substring(0, temp.indexOf(";"));
+    Serial.print("Passowrd: ");
+    Serial.println(pass);
+    temp = temp.substring(temp.indexOf(";") + 1);
+
+    ntpServerName = temp.substring(0, temp.indexOf(";"));
+    Serial.print("NTP Server: ");
+    Serial.println(ntpServerName);
+    temp = temp.substring(temp.indexOf(";") + 1);
+
+    timeZone = atoi(temp.substring(0, temp.indexOf(";")).c_str());
+    Serial.print("TZ: ");
+    Serial.println(timeZone);
+    temp = temp.substring(temp.indexOf(";") + 1);
+
+    // Serial.println(temp);
+}
+
+#pragma region -------- NTP 代码 ----------
+
+const int NTP_PACKET_SIZE = 48;		// NTP时间在消息的前48个字节里
 byte packetBuffer[NTP_PACKET_SIZE]; // 输入输出包的缓冲区
 
 time_t getNtpTime()
@@ -148,7 +256,7 @@ time_t getNtpTime()
         ; // 丢弃以前接收的任何数据包
     Serial.println("Transmit NTP Request");
     // 从池中获取随机服务器
-    WiFi.hostByName(ntpServerName, ntpServerIP);
+    WiFi.hostByName(ntpServerName.c_str(), ntpServerIP);
     Serial.print(ntpServerName);
     Serial.print(": ");
     Serial.println(ntpServerIP);
@@ -173,7 +281,7 @@ time_t getNtpTime()
         }
     }
     Serial.println("No NTP Response :-("); // 无NTP响应
-    return 0;                              // 如果未得到时间则返回0
+    return 0;							   // 如果未得到时间则返回0
 }
 
 // 向给定地址的时间服务器发送NTP请求
@@ -181,9 +289,9 @@ void sendNTPpacket(IPAddress &address)
 {
     memset(packetBuffer, 0, NTP_PACKET_SIZE);
     packetBuffer[0] = 0b11100011; // LI, Version, Mode
-    packetBuffer[1] = 0;          // Stratum, or type of clock
-    packetBuffer[2] = 6;          // Polling Interval
-    packetBuffer[3] = 0xEC;       // Peer Clock Precision
+    packetBuffer[1] = 0;		  // Stratum, or type of clock
+    packetBuffer[2] = 6;		  // Polling Interval
+    packetBuffer[3] = 0xEC;		  // Peer Clock Precision
     // 8 bytes of zero for Root Delay & Root Dispersion
     packetBuffer[12] = 49;
     packetBuffer[13] = 0x4E;
@@ -193,3 +301,4 @@ void sendNTPpacket(IPAddress &address)
     Udp.write(packetBuffer, NTP_PACKET_SIZE);
     Udp.endPacket();
 }
+#pragma endregion
